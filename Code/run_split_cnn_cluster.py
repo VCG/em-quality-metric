96f206153f97587b1ec527e156966ec7c9bf9f13
haehn <haehn@seas.harvard.edu>
2015-12-19 14:02:57 -0500

    Use regal.

--- Code/run_split_cnn_cluster.py
+++ Code/run_split_cnn_cluster.py
@@ -7,7 +7,7 @@ from split_cnn import SplitCNN
 #
 
 s = SplitCNN()
-s._DATA_PATH = '/n/home05/haehn/EMQM_DATA/ac3x75/'
+s._DATA_PATH = '/n/regal/pfister_lab/haehn/'
 s._PATCH_PATH = s._DATA_PATH+'patches_medium/'
 s._EPOCHS = 50
 s._BATCH_SIZE = 500

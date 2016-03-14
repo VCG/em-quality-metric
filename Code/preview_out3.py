765fa74476f3168a5d9522db72d9802db32ca9be
Daniel Haehn <haehn@rclogin09.rc.fas.harvard.edu>
2016-03-14 18:08:17 -0400

    Add the preview scripts.

---
+++ Code/preview_out3.py
@@ -0,0 +1,19 @@
+import glob
+import os
+import time
+
+files = glob.glob('/n/home05/haehn/slurm/out-*.txt')
+for f in files:
+
+  mtime = os.path.getmtime(f)
+
+  if time.time()-mtime > 10000:
+    continue
+  print f
+  with open(f, 'r') as l:
+    lines = l.readlines()
+    #print lines[-5:]
+    for g in lines[-8:]:
+      print g.strip('\n')
+    print '-'*80
+    
